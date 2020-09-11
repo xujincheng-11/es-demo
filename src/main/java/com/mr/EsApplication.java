18d9d49c8d11b3bd38d1400ceaea2bcab1d7eebd
xujincheng <2286870522@qq.com>
2020-09-11 22:08:12 +0800

    33

--- src/main/java/com/mr/EsApplication.java
+++ src/main/java/com/mr/EsApplication.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date 2020/8/4
  * @Version V1.0
  **/
-@SpringBootApplication //启动类额鹅鹅鹅
+@SpringBootApplication //启动类额鹅鹅鹅11111
 public class EsApplication {
     public static void main(String[] args) {
         SpringApplication.run(EsApplication.class);
